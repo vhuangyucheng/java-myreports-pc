74cf7b323f290a77b3bd9ef7d028ef60b98a3c81
vhuangyucheng@gmail.com <vhuangyucheng@gmail.com>
2023-06-30 13:56:26 -0700

    1:56 6/30

--- src/main/java/com/solar4america/SpringBooter.java
+++ src/main/java/com/solar4america/SpringBooter.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBooter {
 
     public static void main(String[] args) {
-        SpringApplication.run(Test.class, args);
+        SpringApplication.run(SpringBooter.class, args);
     }
 
 }
---
+++ src/main/java/com/solar4america/test/Test.java
@@ -0,0 +1,60 @@
+package com.solar4america.test;
+
+import org.eclipse.milo.opcua.sdk.client.OpcUaClient;
+import org.eclipse.milo.opcua.sdk.client.api.config.OpcUaClientConfig;
+import org.eclipse.milo.opcua.sdk.client.api.identity.UsernameProvider;
+import org.eclipse.milo.opcua.sdk.client.nodes.UaVariableNode;
+import org.eclipse.milo.opcua.stack.core.AttributeId;
+import org.eclipse.milo.opcua.stack.core.security.SecurityPolicy;
+import org.eclipse.milo.opcua.stack.core.types.builtin.DataValue;
+import org.eclipse.milo.opcua.stack.core.types.builtin.LocalizedText;
+import org.eclipse.milo.opcua.stack.core.types.builtin.NodeId;
+import org.eclipse.milo.opcua.stack.core.types.builtin.QualifiedName;
+import org.eclipse.milo.opcua.stack.core.types.builtin.unsigned.UInteger;
+import org.eclipse.milo.opcua.stack.core.types.enumerated.TimestampsToReturn;
+import org.eclipse.milo.opcua.stack.core.types.structured.EndpointDescription;
+import org.eclipse.milo.opcua.stack.core.types.structured.ReadResponse;
+import org.eclipse.milo.opcua.stack.core.types.structured.ReadValueId;
+import org.springframework.boot.SpringApplication;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        try {
+            OpcUaClient client = OpcUaClient.create(
+                    "opc.tcp://milo.digitalpetri.com:62541/milo",
+                    endpoints ->
+                            endpoints.stream()
+                                    .filter(e -> e.getSecurityPolicyUri().equals(SecurityPolicy.None.getUri()))
+                                    .findFirst(),
+                    configBuilder ->
+                            configBuilder.build()
+            );
+            client.connect().get();
+
+
+            UaVariableNode testNode = (UaVariableNode) client.getAddressSpace().getNode(
+                    new NodeId(2, "CTT/References/Has3ForwardRefs_1/001")
+            );
+
+// Read the Value attribute
+            DataValue value = testNode.readValue();
+
+// Read the BrowseName attribute
+            QualifiedName browseName = testNode.readBrowseName();
+
+// Read the Description attribute, with timestamps and quality intact
+            DataValue descriptionValue = testNode.readAttribute(AttributeId.Description);
+            System.out.println(descriptionValue + "ddddddd");
+            System.out.println(value + "ddddddd");
+            System.out.println(browseName + "ddddddd");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
