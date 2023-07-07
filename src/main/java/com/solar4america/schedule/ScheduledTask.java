bdc4f6af85bb93d0cd19a9721c56426b7d150ddc
vhuangyucheng@gmail.com <vhuangyucheng@gmail.com>
2023-07-06 17:04:04 -0700

    5:03pm 7/6

---
+++ src/main/java/com/solar4america/schedule/ScheduledTask.java
@@ -0,0 +1,24 @@
+package com.solar4america.schedule;
+
+import lombok.extern.slf4j.Slf4j;
+import lombok.extern.slf4j.XSlf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Configuration
+@EnableScheduling
+@Slf4j
+public class ScheduledTask {
+
+    @Scheduled(cron="0 5 16 * * ?")
+    public void scheduledTask() {
+        System.out.println("任务执行时间：" + LocalDateTime.now());
+        log.info("任务执行时间：" + LocalDateTime.now());
+    }
+
+}
+
